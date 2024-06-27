52b5604d5754ad412aafa709a7da9737dc304bcf
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-28 01:13:56 +0300

    handle errors coming from zod

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,3 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import catchErrors from "../utils/catchErrors";
 import { z } from "zod";
+
+const registerSchema = z
+  .object({
+    email: z.string().email().min(1).max(255),
+    password: z.string().min(6).max(255),
+    confirmPassword: z.string().min(6).max(255),
+    userAgent: z.string().optional(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export const registerHandler = catchErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const request = registerSchema.parse({
+      ...req.body,
+      userAgent: req.headers["user-agent"],
+    });
+  }
+);
--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,8 +1,28 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, Response } from "express";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../constants/http";
+import { z } from "zod";
+
+const handleZodError = (res: Response, error: z.ZodError) => {
+  const errors = error.issues.map((err) => ({
+    path: err.path.join(""),
+    message: err.message,
+  }));
+
+  return res.status(BAD_REQUEST).json({
+    message: error.message,
+    errors,
+  });
+};
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   console.log(`PATH: ${req.path}`, error);
-  return res.status(500).json({ message: "Something went wrong" });
+
+  if (error instanceof z.ZodError) {
+    return handleZodError(res, error);
+  }
+  return res
+    .status(INTERNAL_SERVER_ERROR)
+    .json({ message: "Something went wrong" });
 };
 
 export default errorHandler;
