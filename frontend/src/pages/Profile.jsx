87a571611f727d3b2e60d7272dd73e692383ada9
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-30 02:20:57 +0300

    commit unnecessary code commented

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -7,12 +7,12 @@ const Profile = () => {
   return (
     <Center mt={16} flexDir="column">
       <Heading mb={4}>My Account</Heading>
-      {!verified && (
+      {/* {!verified && (
         <Alert status="warning" w="fit-content" borderRadius={12} mb={3}>
           <AlertIcon />
           Please verify your email
         </Alert>
-      )}
+      )} */}
       <Text color="white" mb={2}>
         Email:{" "}
         <Text as="span" color="gray.300">
